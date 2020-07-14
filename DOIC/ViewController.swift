55a22d86e3e7dd718b81fb9653c94adb55c4dafd
Django <maja0270558@gmail.com>
2020-07-14 12:12:07 +0800

    Init commit

---
+++ DOIC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DOIC
+//
+//  Created by 1293 on 2020/7/14.
+//  Copyright © 2020 maja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
